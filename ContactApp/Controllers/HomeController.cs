e7b15685eace32d2f1357ce580305e258d399da9
TheDanielBrain <danielbrainhh@hotmail.com>
2021-10-12 16:16:46 -0500

    Add project files.

---
+++ ContactApp/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using ContactApp.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ContactRepository cDB = new ContactRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetContacts(string SortOrder = "asc", string SortName = "LastName", int PageSize = 10, int PageNumber = 1, string Search = "")
+        {
+            List<Contact> cList = null;
+            JsonResult jContact = null;
+            int TotalRecords = 0;
+            dynamic jList = new JArray() as dynamic;
+
+            try
+            {
+                int Skip = (PageNumber * PageSize) - PageSize;
+                cList = cDB.GetContacts(SortName, SortOrder, Search, Skip, PageSize, out TotalRecords);
+            }
+            catch (Exception ex)
+            {
+                string Message = ex.Message;
+                return Json(Message, JsonRequestBehavior.AllowGet);
+            }
+
+            jContact = Json(new { total = TotalRecords, rows = cList }, JsonRequestBehavior.AllowGet);
+            jContact.MaxJsonLength = int.MaxValue;
+            return jContact;
+        }
+
+        public JsonResult Save(Contact contact)
+        {
+            JsonResult jResponse = null;
+            bool Status = false;
+
+            try
+            {
+                cDB.SaveContact(contact);
+                Status = true;
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+            }
+
+            jResponse = Json(new { Status = Status }, JsonRequestBehavior.AllowGet);
+            jResponse.MaxJsonLength = int.MaxValue;
+            return jResponse;
+        }
+
+        [HttpPost]
+        public JsonResult GetContactById(int ContactId)
+        {
+            bool Status = false;
+            Contact contact = null;
+            JsonResult jContact = null;
+
+            try
+            {
+                contact = cDB.GetContactById(ContactId);
+                Status = true;
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+            }
+
+            jContact = Json(new { contact = contact, status = Status }, JsonRequestBehavior.AllowGet);
+            jContact.MaxJsonLength = int.MaxValue;
+            return jContact;
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ItemId)
+        {
+            string Success = "";
+            try
+            {
+                Contact usr = cDB.GetContactById(ItemId);
+                cDB.DeleteContact(ItemId);
+                Success = "Success";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = ex.Message;
+                return View("Error", new HandleErrorInfo(ex, "Home", "Delete"));
+            }
+            return Json(Success, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ContactApp/Models/ContactRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Linq.Dynamic;
+
+
+namespace ContactApp.Models
+{
+    public class ContactRepository
+    {
+        private ContactsDBEntities db = new ContactsDBEntities();
+
+        public List<Contact> GetContacts(string SortName, string SortOrder, string Search, int Skip, int PageSize, out int TotalRecords)
+        {
+            List<Contact> cList = null;
+
+
+                cList = db.Contacts
+                    .Where(u =>
+                            u.FirstName.Contains(Search) ||
+                            u.LastName.Contains(Search) ||
+                            u.PhoneNumber.Contains(Search) ||
+                            u.Email.Contains(Search))
+                    .OrderBy(SortName + " " + SortOrder)
+                    .ToList();
+
+            TotalRecords = cList.Count;
+
+            if (PageSize > 0)
+            {
+                cList = cList.Skip(Skip).Take(PageSize).ToList();
+            }
+
+            return cList;
+        }
+
+        public void SaveContact(Contact Contact)
+        {
+            if (Contact.Id == 0)
+            {
+                db.Contacts.Add(Contact);
+                db.SaveChanges();
+            }
+            else
+            {
+                db.Entry(Contact).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public bool DeleteContact(int ContactId)
+        {
+            Contact contact = db.Contacts.Where(c => c.Id == ContactId).FirstOrDefault();
+            bool IsDeleted = false;
+            try
+            {
+                db.Contacts.Remove(contact);
+                db.SaveChanges();
+                IsDeleted = true;
+            }
+            catch (Exception ex)
+            {
+                IsDeleted = false;
+            }
+
+            return IsDeleted;
+        }
+
+        public Contact GetContactById(int ContactId)
+        {
+            Contact contact = db.Contacts.Where(c => c.Id == ContactId).FirstOrDefault();
+            return contact;
+        }
+
+    }
+}
